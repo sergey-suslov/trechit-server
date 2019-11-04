bc91cf66b2e84d934f3adb31ef898cb77f52899b
sergey.suslov <sergey.suslov.nsk@gmail.com>
2019-11-04 17:56:42 +0700

    feat: 🎸 db setup

---
+++ cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	godotenv "github.com/joho/godotenv"
+	db "github.com/sergey-suslov/trechit-server/internal/db"
+	"log"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	_, err = db.Init()
+	if err != nil {
+		log.Println("Couldn't connect to DB")
+	}
+}
---
+++ internal/db/init.go
@@ -0,0 +1,48 @@
+package db
+
+import (
+	"os"
+	"context"
+	"github.com/jackc/pgx"
+	"github.com/jackc/pgx/pgxpool"
+)
+
+// Pool of pgx connections
+var Pool *pgxpool.Pool
+
+func getConnectionString() string {
+	username := os.Getenv("DATABASE_USERNAME")
+	password := os.Getenv("DATABASE_PASSWORD")
+	host := os.Getenv("DATABASE_HOST")
+	port := os.Getenv("DATABASE_PORT")
+	database := os.Getenv("DATABASE_NAME")
+	return "postgresql://" + username + ":" + password + "@" + host + ":" + port + "/" + database
+}
+
+// GetConnection Get connection to DB
+func GetConnection() (*pgx.Conn, error) {
+	conn, err := pgx.Connect(context.Background(), getConnectionString())
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+// GetPool returns pgxpool of connections
+func GetPool() (*pgxpool.Pool, error) {
+	pool, err := pgxpool.Connect(context.Background(), getConnectionString())
+	if err != nil {
+		return nil, err
+	}
+	return pool, nil
+}
+
+// Init db connection pool
+func Init() (*pgxpool.Pool, error) {
+	pool, err := GetPool()
+	if err != nil {
+		return nil, err
+	}
+	Pool = pool
+	return Pool, nil
+}
